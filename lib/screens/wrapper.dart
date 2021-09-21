2c6ae2040d5bb7e9cd163c695a7d3ffaa6ea3eb7
Jake <jkwoodruff93@gmail.com>
2021-09-20 22:51:25 -0700

    changed home page

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shallows/screens/lake_page/lake.dart';
+import 'package:shallows/screens/home/home_page.dart';
 import 'package:shallows/screens/authenticate/sign_in.dart';
 import 'package:shallows/models/myUser.dart';
 import 'package:shallows/services/auth_service.dart';
@@ -16,7 +16,7 @@ class Wrapper extends StatelessWidget {
         builder: (_, AsyncSnapshot<MyUser?> snapshot) {
           if (snapshot.connectionState == ConnectionState.active) {
             final MyUser? myUser = snapshot.data;
-            return myUser == null ? SignIn() : Lake();
+            return myUser == null ? SignIn() : HomePage();
           } else {
             return Scaffold(
               body: Center(
